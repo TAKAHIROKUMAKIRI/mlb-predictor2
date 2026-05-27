4d4a3deb31c57d170a75347e49c4ffb49c42219d
TAKAHIROKUMAKIRI <kumasan0717@icloud.com>
2026-05-27 23:13:33 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "MLB Predictor",
+  description: "MLB速報",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
