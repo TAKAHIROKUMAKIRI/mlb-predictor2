34cc8ed0c56c005c8cc12bd0c866bf64eadc892f
TAKAHIROKUMAKIRI <kumasan0717@icloud.com>
2026-06-06 21:45:23 +0900

    Add homeRecentPitcherForm bonus calculation

--- app/page.tsx
+++ app/page.tsx
@@ -226,11 +226,13 @@ function winProbability(game: any) {
   (bullpenBonus(game.awayMetrics, game.awayBullpen) || 0) +
   (matchupBonus(game) || 0) +
   (game.headToHead?.bonus || 0) -
+    (game.awayRecentPitcherForm?.bonus || 0)
   (
     (strength(game.homeMetrics, game.homePitcherMetrics) || 0) +
     (recentFormBonus(game.homeRecentForm) || 0) +
     (homeAwayBonus(game.homeHomeRecord) || 0) +
     (bullpenBonus(game.homeMetrics, game.homeBullpen) || 0) +
+    (game.homeRecentPitcherForm?.bonus || 0)
     homeAdv
   );
 
